a45f7bbc89476ca92d4de380cfb0c2ba76aa79b6
Vujadin Dagovic <vujke456@gmail.com>
2023-08-18 17:57:26 +0200

    implemented lazy loading for images in CardDetail component

--- src/components/CardDetail/CardDetail.component.jsx
+++ src/components/CardDetail/CardDetail.component.jsx
@@ -4,8 +4,10 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 import "../../styles/cardDetail.css";
+import { useState } from "react";
 
 const CardDetail = ({ project }) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
   const {
     title,
     imageUrl,
@@ -14,9 +16,20 @@ const CardDetail = ({ project }) => {
     liveDemoLink = "",
     certificateLink = "",
   } = project;
+
+  const loaded = () => setIsImageLoaded(true);
+
   return (
-    <div className="card-wrapper">
-      <img loading="lazy" className="card-img" src={imageUrl} alt="" />
+    <div className={`card-wrapper`}>
+      <div className={`card-img-wrap ${!isImageLoaded ? "loadingImg" : ""}`}>
+        <img
+          loading="lazy"
+          className="card-img"
+          src={imageUrl}
+          alt=""
+          onLoad={loaded}
+        />
+      </div>
       <h3 className="card-title">{title}</h3>
       {description && <p className="card-desc">{description}</p>}
       <div className="card-button-container">
